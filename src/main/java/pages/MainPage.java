a432e12559fea24cf83476b08ff923c497126a5d
Yury Piskun <singleshooting@gmail.com>
2022-09-12 19:38:34 +0300

    fix test

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -20,12 +20,12 @@ public class MainPage extends BasePage {
 
     public void openProductQuickView(String productName) {
         String productXpath = String.format("//ul[@id='homefeatured']//a[@title='%s' and @class='product-name']", productName);
-        String quickViewButtonXpath = productXpath + "/preceding::span[text()='Quick view'][1]";
+        String quickViewButtonXpath = productXpath + "/ancestor::div[@class='product-container']//span[text()='Quick view']";
         WebElement necessaryProduct = driver.findElement(By.xpath(productXpath));
-        WebElement quickViewButton = driver.findElement(By.xpath(quickViewButtonXpath));
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", quickViewButton);
+        js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(By.xpath(quickViewButtonXpath)));
         actions.moveToElement(necessaryProduct).build().perform();
+        WebElement quickViewButton = driver.findElement(By.xpath(quickViewButtonXpath));
         quickViewButton.click();
     }
 

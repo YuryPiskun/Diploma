329028707b3854b92c4b2496756afe0947763967
Yury Piskun <singleshooting@gmail.com>
2022-09-12 18:57:39 +0300

    edit openProductQuickView

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import utils.PropertiesLoader;
@@ -21,9 +22,10 @@ public class MainPage extends BasePage {
         String productXpath = String.format("//ul[@id='homefeatured']//a[@title='%s' and @class='product-name']", productName);
         String quickViewButtonXpath = productXpath + "/preceding::span[text()='Quick view'][1]";
         WebElement necessaryProduct = driver.findElement(By.xpath(productXpath));
-        actions.moveToElement(necessaryProduct).build().perform();
         WebElement quickViewButton = driver.findElement(By.xpath(quickViewButtonXpath));
-        actions.moveToElement(quickViewButton).build().perform();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", quickViewButton);
+        actions.moveToElement(necessaryProduct).build().perform();
         quickViewButton.click();
     }
 
